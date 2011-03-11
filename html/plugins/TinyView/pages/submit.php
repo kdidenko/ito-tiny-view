cd3f735bc998f7d29a367746be00235dc2dc74d9
kdidenko <kdidenko@gmail.com>
2011-03-11 10:50:53 +0000

    Initial commit

--- html/plugins/TinyView/pages/submit.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$t_plugin_path = config_get( 'plugin_path' ). 'TinyView' . DIRECTORY_SEPARATOR;
-	require_once( $t_plugin_path . 'core' . DIRECTORY_SEPARATOR . 'Helper.php' );
-	$helper = Helper::getInstance();
-	$user = $_REQUEST['user'];
-	$project =  $_REQUEST['project'];
-	$helper->add($user, $project);
-?>
